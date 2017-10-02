d686d445e98c2a5ebb154da4dc94482c03253e02
UserNameIsAlredyTaken <gdaniltatarinof@gmail.com>
2017-10-02 12:18:10 +0300

    Ввод масива завершается по cntrl+D, написал все функции высших порядков, кроме итератора

--- LinkedList/LinkedList/LinkedList.c
+++ LinkedList/LinkedList/LinkedList.c
@@ -82,19 +82,55 @@ int list_sum(const struct linked_list list){
 	return sum;
 }
 
+void foreach(struct linked_list* list, void (*func)(int)){
+	struct linked_list_node* the_node = list->the_first;
+	func(the_node->value);
+	for(int i =0; i<list->length-1; i++)	{
+		the_node = the_node->next;
+		func(the_node->value);
+	}
+}
+
+struct linked_list map(struct linked_list* old_list, int (*func)(int)){
+	struct linked_list* new_list = list_create();
+	struct linked_list_node* old_node = old_list->the_first;
+	list_add_back(func(old_node->value), &new_list);
+	for (int i = 0; i<old_list->length - 1; i++) {
+		old_node = old_node->next;
+		list_add_back(func(old_node->value), &new_list);
+	}
+	return *new_list;
+}
+
+struct linked_list map_mut(struct linked_list* list, int (*func)(int)){
+	struct linked_list_node* the_node = list->the_first;
+	the_node->value = func(the_node->value);
+	for(int i =0; i<list->length-1; i++){
+		the_node = the_node->next;
+		the_node->value = func(the_node->value);
+	}
+	return *list;
+}
+
+int foldl(int acc, struct linked_list* list, int (*func)(int, int)) {
+	struct linked_list_node* the_node = list->the_first;
+	acc = func(the_node->value, acc);
+	for (int i = 0; i<list->length - 1; i++) {
+		the_node = the_node->next;
+		acc = func(the_node->value, acc);
+	}
+	return acc;
+}
+
+
+
 int main() {
 	struct linked_list* the_list = list_create();
 	int input=0;
-	printf("Enter the array\n");
+	printf("Enter the array\n"); //input finishes with Cntrl+D
 	while(scanf_s("%d", &input)!=0){
 		list_add_front(input, &the_list);
 	}
-	/*scanf_s("%d", &input);
-	list_add_front(input, &the_list);
-	scanf_s("%d", &input);
-	list_add_front(input, &the_list);
-	scanf_s("%d", &input);
-	list_add_front(input, &the_list);*/
 	printf("The summ is: %d\n", list_sum(*the_list));	
 	list_free(*the_list);
 }
